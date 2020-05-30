065884a416493a2579e83026977262d395e15c0d
Tushar Mohod <tusharmohod33@gmail.com>
2020-05-30 19:47:40 +0530

    Print message in add method

--- calculator.py
+++ calculator.py
@@ -1,11 +1,12 @@
 def addition(x, y):
-	return x+y
+    print("this is addition")
+    return x+y
 
 def subtraction(x, y):
-	return x-y
+    return x-y
 
 def multiplication(x, y):
-	return x*y
+    return x*y
 
 def divide(x, y):
-	return x/y
+    return x/y
